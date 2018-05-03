91eadb08a7958ba761cc2e66873c4365dcc19fca
Sam Kome <skome@canecreekstudio.org>
2018-05-03 06:56:38 -0700

    Added comments and docstrings

--- hathi_Records.py
+++ hathi_Records.py
@@ -2,7 +2,6 @@
 
 import json
 import csv
-#import urllib2
 import requests
 import sys
 
@@ -21,10 +20,13 @@ Provided a file of OCLC numbers, return ISBN(s)
 hURL_brief = 'http://catalog.hathitrust.org/api/volumes/brief/oclc/'
 hURL_full = 'http://catalog.hathitrust.org/api/volumes/full/oclc/'
 
-oNum = ''
 hStub = '.json'
+DELIMITER = '|'
+QUOTE = '"'
 
-class codesList:
+class codesList: # list maker
+    """from an open file of (single column) strings, return a 
+    list of strings stripped of leading and trailing whitespace"""
     def __init__(self, fh):
         self.codeFile = fh
     def listed(self):
@@ -33,12 +35,14 @@ class codesList:
             lines.append(line.strip())
         return lines
 
-def print_status(numcodes, totalNum, msg): #progress indicator
+def print_status(numcodes, totalNum, msg): # progress indicator
     """status printing utility for long-running scripts"""
     print('Record: {} / {} {:>20}\r'.format(numcodes, totalNum, msg), end='\r'),
     sys.stdout.flush()  
 
-def lookup(url):
+def lookup(url): # URL retriever (expects json data)
+    """accepts a url of json data, retreives and parses into 
+    plaintext"""
     try:
         h_response = requests.get(url)
         txtResult = h_response.text
@@ -54,7 +58,7 @@ def lookup(url):
         print("Unexpected error:", sys.exc_info()[0])
         return ""
 
-def get_hathi_rec_report(oclc_num, h_result):
+def get_hathi_rec_report(oclc_num, h_result): # hathi data parser
     """Accept a dictionary of hathi fields and return a list
     that includes catalog url, item url, and rights"""
     # Hathi data consists of records and items. A record is a description 
@@ -77,28 +81,28 @@ def get_hathi_rec_report(oclc_num, h_result):
                 csv_rows.append(csv_row)
     return csv_rows
 
-def search(lCodes): 
-    numo=len(lCodes)
-    csv_rows = []
+def search(lCodes): # search wrapper: OCLC nums to get hathi data
+    """from a list of oclc numbers, return csv'd Hathi data """
+    numo=len(lCodes) # dataset size for status updates
+    csv_rows = [] # holds result data
     for i,oclc_num in enumerate(lCodes): # Iterate through the codes
         print_status(i,numo,oclc_num)
-        hReq = hURL_brief+oclc_num+hStub
-        queryResult = lookup(hReq)
-        csv_row = get_hathi_rec_report(oclc_num, queryResult)            
-        #print(csv_row)
-        if len(csv_row)>0:
+        hReq = hURL_brief+oclc_num+hStub # build search URI
+        queryResult = lookup(hReq) # call Hathi
+        csv_row = get_hathi_rec_report(oclc_num, queryResult)  # make csv data row(s)
+        if len(csv_row)>0: # if not null, add to dataset of rows
             csv_rows.append(csv_row)
     return csv_rows
 
 if __name__ == "__main__":        
-    fileIn = sys.argv[1] #                          get the text data to process
-    fileOut = sys.argv[2]    #                      put results here
+    fileIn = sys.argv[1] # get the text data to process
+    fileOut = sys.argv[2] # put results here
     with open(fileIn,'r') as inputF, open(fileOut,'w', newline='') as outputF:
-        csvw = csv.writer(outputF,delimiter=',', quotechar='"')  # csv writing machine
+        csvw = csv.writer(outputF,delimiter=DELIMITER, quotechar=QUOTE)  # csv writing machine
         csvw.writerow(['OCLCNUM','Hathi_catalog_entry','hathi_item_url', 'rights'])
-        codeList = codesList(inputF) #                prep a list object
-        codes = codeList.listed() #                    read file into a list, whitespace removed    
-        results = search(codes) #                      DO THE ACTUAL LOOKUPS  
-        for row in results:
-            for r in row:
+        codeList = codesList(inputF) # make a list from the input file
+        codes = codeList.listed() # read file into a list, whitespace removed    
+        results = search(codes) # look up all the items  
+        for row in results: # write results to file
+            for r in row: # some rows are compound (multiple records/row)
                 csvw.writerow(r)
